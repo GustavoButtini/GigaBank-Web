92f0e903dc065924280c0e37b5a0f632a02f208f
Gustavo Buttini <102230571+GustavoButtini@users.noreply.github.com>
2023-09-06 08:51:58 -0300

    Criado usando o Colaboratory

---
+++ Grupo_De_Estudo.py
@@ -0,0 +1,74 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMLGuwJZFjER96awHSIv3FZ",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/GustavoButtini/GigaBank-Web/blob/main/Grupo_De_Estudo.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 3,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "K5qVkEfEQmwY",
+        "outputId": "6a54a2f1-4e77-40e0-883d-a15676fdf572"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Esse texto é codigo !!\n"
+          ]
+        }
+      ],
+      "source": [
+        "print(\"Esse texto é codigo !!\")\n",
+        "#print(\"ola\")"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "#Esse texto não é codigo !!!\n",
+        "print(\"ola\")\n",
+        "\n"
+      ],
+      "metadata": {
+        "id": "Ai6kLfwyQtWm"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "WzCb-uirRmRt"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
